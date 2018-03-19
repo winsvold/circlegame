42b22b48fa296734ba36fc532cf9b1f094430f22
Daniel Winsvold <danielwinsvold@gmail.com>
2018-03-19 23:23:54 +0100

    Added full screen button

--- js/resize.js
+++ js/resize.js
@@ -16,3 +16,46 @@ function resizeGame() {
 window.setInterval(function () {
     resizeGame();
 }, 200);
+
+var requestFullscreen = function (ele) {
+    if (ele.requestFullscreen) {
+        ele.requestFullscreen();
+    } else if (ele.webkitRequestFullscreen) {
+        ele.webkitRequestFullscreen();
+    } else if (ele.mozRequestFullScreen) {
+        ele.mozRequestFullScreen();
+    } else if (ele.msRequestFullscreen) {
+        ele.msRequestFullscreen();
+    } else {
+        console.log('Fullscreen API is not supported.');
+    }
+};
+
+var exitFullscreen = function () {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    } else {
+        console.log('Fullscreen API is not supported.');
+    }
+};
+
+var elem = document.body;
+
+$('.full-screen').click(function () {
+    if (
+        document.fullscreenElement ||
+        document.webkitFullscreenElement ||
+        document.mozFullScreenElement ||
+        document.msFullscreenElement
+    ) {
+        exitFullscreen()
+    } else {
+        requestFullscreen(elem);
+    }
+});
